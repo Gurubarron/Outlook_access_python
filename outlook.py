290edd1003d0023ff12084c609f053328cdc7a7d
Gurubarron <70693597+Gurubarron@users.noreply.github.com>
2021-06-22 21:06:33 +0530

    Update outlook.py

--- outlook.py
+++ outlook.py
@@ -1,3 +1,4 @@
+opt1:
 import win32com.client
 import os
 get_path = os.getcwd()
@@ -14,3 +15,21 @@ for m in messages:
         for x in message2.Attachments:
             x.SaveASFile(os.path.join(get_path,x.FileName))
             print "successfully downloaded attachments"
+            
+            
+#opt 2:
+from exchangelib import DELEGATE, Account, Credentials
+
+credentials = Credentials(
+    username='MYWINDOMAIN\\myusername',  # Or myusername@example.com for O365
+    password='topsecret'
+)
+account = Account(
+    primary_smtp_address='john@example.com', 
+    credentials=credentials, 
+    autodiscover=True, 
+    access_type=DELEGATE
+)
+# Print first 100 inbox messages in reverse order
+for item in account.inbox.all().order_by('-datetime_received')[:100]:
+    print(item.subject, item.body, item.attachments)
